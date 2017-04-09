aa6628c5a2a7d8b0932f76b3e1a0b10ec87e42b2
github username <github email address>
2017-04-08 22:42:27 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,26 +1,35 @@
+require 'pry'
+
 class School
+attr_accessor :name
 
-  attr_accessor :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
 
-def initialize(name)
-  @name = name
-  @roster = {}
-end
+  def roster
+    @roster
+  end
 
+  def add_student(student, grade)
+    @roster[grade] ||= []
+    # if a grade already exists as a key within the roster hash,
+    # then @roster[grade] = @roster[grade] and the student
+    # will be pushed into the array that is the value of this key.
+    # if @roster[grade] is false, nil, or undefined,
+    # then @roster[grade] = to an empty array
+    @roster[grade]<< student
+  end
 
-def add_student(name, grade)
-  roster[grade] ||= []
-  roster[grade] << name
-end
+  def grade(grade)
+    @roster[grade]
+  end
 
-def grade(grade)
-  roster[grade]
-end
-
-def sort
-  @roster.collect {|grade, student|
-    @roster[grade] = student.sort}
-  @roster
-end
-
+  def sort
+    sorted_hash = {}
+    @roster.each {|grade, student|
+    sorted_hash[grade] = student.sort}
+    sorted_hash
+  end
 end
